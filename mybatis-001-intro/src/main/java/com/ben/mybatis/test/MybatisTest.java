be01233aac824a009cd002102cf221a405786aec
benjieqiang <765126548@qq.com>
2023-03-30 20:16:56 +0800

    mybatis insert car

--- mybatis-001-intro/src/main/java/com/ben/mybatis/test/MybatisTest.java
+++ mybatis-001-intro/src/main/java/com/ben/mybatis/test/MybatisTest.java
@@ -1,48 +1,32 @@
 package com.ben.mybatis.test;
 
-public class MybatisTest {
-    public static void main(String[] args) {
-
-    }
-}
-
-
-package com.powernode.mybatis.test;
-
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
-public class MyBatisIntroductionTest {
-    public static void main(String[] args) throws Exception {
-
-        // 获取SqlSessionFactoryBuilder对象
+public class MybatisTest {
+    public static void main(String[] args) throws IOException {
+        //1. 创建SqlSessionFactoryBuilder对象；
         SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        //2. SqlSessionFactory对象, 需要抛出异常；
+//        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+//        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        // 也可以写成一行；
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(Resources.getResourceAsStream("mybatis-config.xml"));
 
-        // 获取SqlSessionFactory对象
-        InputStream is = Resources.getResourceAsStream("mybatis-config.xml"); // Resources.getResourceAsStream默认就是从类的根路径下开始查找资源。
-        //InputStream is = Resources.getResourceAsStream("com/mybatis.xml");
-        //InputStream is = new FileInputStream("d:\\mybatis-config.xml");
-
-        //InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("mybatis-config.xml");
-        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is); // 一般情况下都是一个数据库对应一个SqlSessionFactory对象。
-
-        // 获取SqlSession对象
+        //3. 创建SqlSession对象
         SqlSession sqlSession = sqlSessionFactory.openSession(); // 如果使用的事务管理器是JDBC的话，底层实际上会执行：conn.setAutoCommit(false);
-        // 这种方式实际上是不建议的，因为没有开启事务。
-        //SqlSession sqlSession = sqlSessionFactory.openSession(true);
-
-        // 执行SQL语句
-        int count = sqlSession.insert("insertCar"); // 返回值是影响数据库表当中的记录条数。
-
-        System.out.println("插入了几条记录：" + count);
-
-        // 手动提交
-        sqlSession.commit(); // 如果使用的事务管理器是JDBC的话，底层实际上还是会执行conn.commit();
-
+        //4. 执行sql语句
+        int res = sqlSession.insert("insertCar");
+        //5. 打印结果
+        System.out.println("插入结果是：" + res);
+        //6. 提交结果
+        sqlSession.commit();
+        //7. 关闭资源
+        sqlSession.close();
     }
 }
