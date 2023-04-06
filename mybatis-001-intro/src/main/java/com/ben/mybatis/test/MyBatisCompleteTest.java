5de6d59cbdbf49bee098d488f924e41120d46b78
benjieqiang <765126548@qq.com>
2023-04-06 10:10:57 +0800

    mybatis intro

---
+++ mybatis-001-intro/src/main/java/com/ben/mybatis/test/MyBatisCompleteTest.java
@@ -0,0 +1,39 @@
+package com.ben.mybatis.test;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+
+public class MyBatisCompleteTest {
+
+    public static void main(String[] args) throws IOException {
+        SqlSession sqlSession = null;
+        try {
+            //1. 创建SqlSessionFactoryBuilder对象；
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+            //2. SqlSessionFactory对象, 需要抛出异常；
+            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(Resources.getResourceAsStream("mybatis-config.xml"));
+            //3. 创建SqlSession对象
+            sqlSession = sqlSessionFactory.openSession(); // 如果使用的事务管理器是JDBC的话，底层实际上会执行：conn.setAutoCommit(false);
+            //4. 执行sql语句
+            int res = sqlSession.insert("insertCar");
+            System.out.println("插入结果是：" + res);
+            //5. 提交结果
+            sqlSession.commit();
+
+        } catch (Exception e) {
+            // 回滚
+            if (sqlSession != null) {
+                sqlSession.rollback();
+            }
+        } finally {
+            // 6.关闭资源
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+}
